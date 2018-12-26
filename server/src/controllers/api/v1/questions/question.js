f8b5ea5c7ca3a65ad52375b8677659d7ba58b33c
Amriesgrace <obieri.grace@gmail.com>
2018-12-26 10:32:17 +0100

    [feature #162831671] Add votes on a question

--- server/src/controllers/api/v1/questions/question.js
+++ server/src/controllers/api/v1/questions/question.js
@@ -17,8 +17,59 @@ const createQuestion = (req, res) => {
     });
 };
 
+
+const upvote = (req, res) => {
+    const reqId = req.params.id;
+    const result = AllQuestions.find(question => question.id == reqId);
+    if (result) {
+        const newResult = {
+            meetup: result.meetupId,
+            title: result.title,
+            question: result.body,
+            votes: result.votes + 1,
+        };
+        res.status(200).json({
+            status: 200,
+            data: [newResult],
+            // vote: newResult,
+        });
+    } else {
+        res.status(404).json({
+            status: 404,
+            message: 'no question with this id',
+        });
+    }
+};
+
+
+const downvote = (req, res) => {
+    const reqId = req.params.id;
+    const result = AllQuestions.find(question => question.id == reqId);
+    if (result) {
+        const newResult = {
+            meetup: result.meetupId,
+            title: result.title,
+            question: result.body,
+            votes: result.votes - 1,
+        };
+        AllQuestions.push(newResult);
+        res.status(200).json({
+            status: 200,
+            data: [newResult],
+        });
+    } else {
+        res.status(404).json({
+            status: 404,
+            message: 'no question with this id',
+        });
+    }
+};
+
+
 const Questions = {
     createQuestion,
+    upvote,
+    downvote,
 };
 
 export default Questions;
--- server/src/models/questionsRecord.js
+++ server/src/models/questionsRecord.js
@@ -4,14 +4,16 @@ const questionsRecord = [
         meetupId: 10011,
         meetupTitle: 'Gryffindor meets',
         title: 'Enquiries',
-        body: 'When is the next meeting'
+        body: 'When is the next meeting',
+        votes: 1,
     },
     {
         id: 2,
         meetupId: 10011,
         meetupTitle: 'Gryffindor meets',
         title: 'Enquiries',
-        body: 'When is the next meeting'
+        body: 'When is the next meeting',
+        votes: 1
     },
 ];
 
