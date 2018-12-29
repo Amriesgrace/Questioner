45ae5824b6fbcf03779015123b2723de091bd804
Amriesgrace <obieri.grace@gmail.com>
2018-12-29 18:42:20 +0100

    [ch test endpoints 162843931] Add test for endpoints

--- server/src/controllers/api/v1/questions/question.js
+++ server/src/controllers/api/v1/questions/question.js
@@ -10,8 +10,8 @@ const createQuestion = (req, res) => {
     };
     AllQuestions.push(newQuestion);
 
-    res.status(201).json({
-        status: 201,
+    res.status(200).json({
+        status: res.statusCode,
         message: 'new question posted',
         data: newQuestion,
     });
@@ -29,7 +29,7 @@ const upvote = (req, res) => {
             votes: result.votes + 1,
         };
         res.status(200).json({
-            status: 200,
+            status: res.statusCode,
             data: [newResult],
             // vote: newResult,
         });
